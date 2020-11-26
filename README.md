87e4ee187b82a5fec59f725352837513fa521d71
Arjan <69347644+skarjan@users.noreply.github.com>
2020-11-26 15:32:14 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
+###LANGVERWACHTE VERSIE 0.7 NU BESCHIKBAAR!!!!
+
+# Versie 0.7 notes
+- Hele code opnieuw geschreven zodat ik makkelijker functies kan toevoegen. 
+- Programma loopt nu van het opvangen van de knoppen tot en met het bijwerken van de scores. 
+Daartussen in heb ik een aantal functies geplaatst. Deze worden aangeroepen op het moment dat 
+één van de invoer knoppen word ingedrukt. Hierop begint de functie speelSpel. Hier wordt de 
+opgegeven waarde vastgelegd. Met een andere functie wordt de invoer van de CPU gegenereerd. 
+Deze functies geven 2 waardes terug die ik vervolgens met elkaar vergelijk. 
+Dit volgens de spelregels. Na het bepalen van de winnaar wordt de score bijgewerkt en melding gedaan van het resultaat. 
+
 # SteenPapierSchaar
 ![Voorbeeld van SPS](https://github.com/skarjan/SteenPapierSchaar/blob/main/img/bijv.png)
 Speel SteenPapierSchaar (SPS) en kijk of je van de "AI" kan winnen. 
+
+
 
 # versie 0.6 notes
 ## spel logica
--- js/script.js
+++ js/script.js
@@ -1,9 +1,84 @@
-// const knoppen = document.getElementsByClassName("std-knop");
-// const doelwit = document.getElementById("knopHouder");
-//
-// doelwit.addEventListener("click", klikFunctie);
-//
-function f1(knop){
-  knopWaarde = knop.value;
-    console.log(knopWaarde);
-}
+  const bericht = document.querySelector('.bericht');
+  const score = document.querySelector('.score');
+  const buttons = document.querySelectorAll('button');
+  const winnaarScores = [0,0];
+
+  //event listeners toevoegen aan alle knoppen
+  for ( let i = 0 ; i < buttons.length ; i++){
+      buttons[i].addEventListener('click', playGame);
+  }
+
+  function playGame(e){
+      //invoer van speler koppelen aan de betreffende knoppen
+      let spelerInvoer = e.target.innerText;
+      // Functie berekend een willekeurige getal tussen 0 en 1
+      let cpuInvoer = Math.random();
+
+      //als waarde < 34, CPU selects Steen <= Steen en de rest schaar
+      if (cpuInvoer < .34){
+          cpuInvoer = 'Steen';
+      } else if (cpuInvoer <= .67){
+          cpuInvoer = 'Papier';
+      } else {
+          cpuInvoer = 'Schaar';
+      }
+
+      // Functie om de invoeren te vergelijken en winnaar te bepalen.
+      let result = checkWinner(spelerInvoer, cpuInvoer);
+
+      //scoren weergeven in DOM
+      if (result === 'Speler'){
+          result += ' wint!!';
+          //score bijwerken
+          winnaarScores[0]++;
+      }
+
+      if (result === 'CPU'){
+          result += ' wint!!';
+          winnaarScores[1]++;
+      }
+
+      if (result === 'Gelijkspel'){
+          result += '. Helaas...'
+      }
+
+      //score weergeven in de betreffende score div
+      score.innerHTML = 'Speler: [ ' + winnaarScores[0]+ ' ] CPU: [ ' + winnaarScores[1] + ' ]';
+
+      //weergeven wat speler en CPU hebben ingevoerd
+      messenger('Speler: <strong>' + spelerInvoer + '<br></strong> CPU: <strong>' + cpuInvoer + '</strong><br>' + result);
+  }
+
+  function messenger(selectionbericht){
+      bericht.innerHTML = selectionbericht;
+  }
+
+  function checkWinner(Speler, CPU){
+      if (Speler === CPU){
+          return 'Gelijkspel';
+      }
+
+      if (Speler === 'Steen'){
+          if(CPU === 'Papier'){
+              return 'CPU';
+          } else {
+              return 'Speler';
+          }
+      }
+
+      if (Speler === 'Papier'){
+          if (CPU === 'Schaar'){
+              return 'CPU';
+          } else {
+              return 'Speler';
+          }
+      }
+
+      if (Speler === 'Schaar'){
+          if (CPU === 'Steen'){
+              return 'CPU';
+          } else {
+              return 'Speler';
+          }
+      }
+  }
